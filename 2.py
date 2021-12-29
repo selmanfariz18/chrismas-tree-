22648d9805d891e1f6a819dd0b866cd5c85251be
selmanfariz18 <92449535+selmanfariz18@users.noreply.github.com>
2021-12-29 11:55:35 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,18 @@
+n=20
+for i in range(1,n+1):
+    print(' '*(n-i)+'* '*i)
+for i in range(int(n*0.35)):
+    for j in range(int(n-4)):
+        print('',end=' ')
+    print('* * * *')
+for i in range(n):
+    print('_ ',end='')
+print()
+print()
+print('~~~~~~~~~~~~HAPPY CHRISTMAS~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~~~~~~AND~~~~~~~~~~~~~~~~~~')
+print('~~~~~~~~~~~~~HAPPY NEWYEAR~~~~~~~~~~~~~')
+print('Created by '
+      'SELMANUL FARIZY -- '
+      'S1 CSE -- '
+      'KMEA ENGINEERING COLLEGE')
